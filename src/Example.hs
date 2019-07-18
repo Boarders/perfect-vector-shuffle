fc9a2cebc3ed74fd9b32731dd264bdf435a0f12f
Boarders <callan.mcgill@gmail.com>
2019-07-18 10:35:04 -0400

    Add example as part of project

--- src/Example.hs
+++ src/Example.hs
@@ -1,10 +1,23 @@
-module Example where
+module Main where
 
 import           Data.Vector
 import           Immutable.Shuffle
 
-shuffleMyVector :: IO (Vector Int)
-shuffleMyVector = shuffleM myVector
+main :: IO ()
+main =
+  do
+    shuffleMyVector >>= print
+    cycleMyVector   >>= print
+    derangeMyVector >>= print
 
 myVector :: Vector Int
 myVector = fromList [1..10]
+
+shuffleMyVector :: IO (Vector Int)
+shuffleMyVector = shuffleM myVector
+
+cycleMyVector :: IO (Vector Int)
+cycleMyVector = maximalCycleM myVector
+
+derangeMyVector :: IO (Vector Int)
+derangeMyVector = derangementM myVector
