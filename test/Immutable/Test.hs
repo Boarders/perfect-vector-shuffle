c8171cc8f0fdf957744d0c71da08f3b59ba2bb59
Boarders <callan.mcgill@gmail.com>
2019-07-18 10:37:40 -0400

    Add test for derangement and maximal cycle

--- test/Immutable/Test.hs
+++ test/Immutable/Test.hs
@@ -5,10 +5,9 @@ module Immutable.Test
   ) where
 
 import           Data.List                        (sort)
-import           Data.Vector                      (Vector)
+import           Data.Vector                      (Vector, (!))
 import qualified Data.Vector                      as V
 import           Immutable.Shuffle
-import           System.Random
 import           Test.QuickCheck.Instances.Vector ()
 import           Test.QuickCheck.Monadic
 import           Test.Tasty
@@ -17,8 +16,10 @@ import           Test.Tasty.QuickCheck            as QC hiding (shuffle)
 
 testSuite :: TestTree
 testSuite = testGroup ""
-  [ localOption (QuickCheckTests 10000) shuffleIOTestSuite
-  , localOption (QuickCheckTests 10   ) performanceTest
+  [ localOption (QuickCheckTests 10000) shuffleMTestSuite
+  , localOption (QuickCheckTests 0    ) performanceTest
+  , localOption (QuickCheckTests 10000) maximalCycleTestSuite
+  , localOption (QuickCheckTests 10000) derangementTestSuite
   ]
 
 performanceTest :: TestTree
@@ -27,11 +28,24 @@ performanceTest = testGroup "Performance"
       "Shuffling preserves length and elements"
       (monadicIO . sameLength)]
 
-shuffleIOTestSuite :: TestTree
-shuffleIOTestSuite = testGroup "shuffleIO"
+shuffleMTestSuite :: TestTree
+shuffleMTestSuite = testGroup "shuffleM"
   [ QC.testProperty
       "Shuffling preserves length and elements"
       (monadicIO . isPermutation @Int)]
+
+maximalCycleTestSuite :: TestTree
+maximalCycleTestSuite = testGroup "maximalCycleM"
+  [ QC.testProperty
+      "maximal cycle does indeed produce a maximal cycle on [0..n]"
+      (monadicIO . isMaximalCycle)]
+
+
+derangementTestSuite :: TestTree
+derangementTestSuite = testGroup "derangementM"
+  [ QC.testProperty
+      "derangement does indeed produce a derangment on [0..n]."
+      (monadicIO . isDerangement)]
 
 sameLength :: () -> PropertyM IO Property
 sameLength _ = do
@@ -48,3 +62,28 @@ isPermutation v =
     let ls' = V.toList v'
     pure $ (sort ls) === (sort ls')
 
+
+isMaximalCycle :: Positive Int -> PropertyM IO Property
+isMaximalCycle (Positive n) =
+  do
+    v <- run $ maximalCycleM (V.fromList [0..n])
+    pure $ cycleLength v === (n + 1)
+
+   where
+     cycleLength :: Vector Int -> Int
+     cycleLength v = go (V.head v) v
+
+     go :: Int -> Vector Int -> Int
+     go k xs = if k == 0
+                       then 1
+                     else
+                       1 + go (xs ! k) xs
+
+isDerangement :: Positive Int -> PropertyM IO Property
+isDerangement (Positive n) =
+  do
+    v <- run $ derangementM (V.fromList [0.. n])
+    let perm    = V.indexed v
+    let unmoved = V.filter (uncurry (==)) perm
+    pure $ (null unmoved) === True
+      
